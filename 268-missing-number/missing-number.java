66a56c4940bcb30710e1ec3ce5d43378cb4bd85b
Sanskar Agrawal <agrawalsanskar60@gmail.com>
2026-08-02 19:50:04 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (92.42%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,3 +1,11 @@
+//LOGIC
+//[2,3,5,0,1], output(4)
+//logic 1- sum=n(n+1)/2 = tota sum-actual sum = 15-11 = 4 (5(5+1)/2)=4
+//logic 2- (XOR[0,n] ^ XOR array)  = [A^0 = A]
+                                    // [A ^A = 0]
+// (0^1^2^3^4^5) ^ (2^3^5^0^1)= by the use of property all cancel with each other
+// 4^0=4 _____[A^0 = A]----By this property-------(1)
+
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
