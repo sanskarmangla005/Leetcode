15b9099be4e96404ce0c39a3ec07e3815af13727
Sanskar Agrawal <agrawalsanskar60@gmail.com>
2026-08-14 10:50:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (48.73%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -6,18 +6,37 @@ class Solution {
 // Agar current element target se bada hai to wahi target ki position hogi
 // Agar kahi nhi mila to last me insert hoga
 
-         for (int i = 0; i < nums.length; i++) {
-            //Target mil gaya
-            if (nums[i] == target) {
-                return i;
-            }
+//          for (int i = 0; i < nums.length; i++) {
+//             //Target mil gaya
+//             if (nums[i] == target) {
+//                 return i;
+//             }
             
-            //Target se bada element mil gaya
-            if (nums[i] > target) {
-                return i;
-            }
-        }
+//             //Target se bada element mil gaya
+//             if (nums[i] > target) {
+//                 return i;
+//             }
+//         }
 
-        return nums.length;
+//         return nums.length;
+//     }
+// }
+
+
+int low = 0;
+int high = nums.length -1;
+while(low <= high){
+    int mid = (low + high)/2;
+    if(nums[mid] == target){
+        return mid;
+    }
+    else if(nums[mid] < target){
+        low = mid + 1;
+    }
+    else {
+        high = mid -1;
     }
 }
+    return low;
+}
+    }
